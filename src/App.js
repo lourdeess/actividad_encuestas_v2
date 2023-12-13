2f1439498943477aa7075a4ed9ffedd9c814ae06
lourdeess <herreralourdesjazmin@gmail.com>
2023-12-12 21:32:25 -0300

    actividad_encuestas_v2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./Components/Inicio";
+import CrearEncuesta from "./Components/Crearencuestas"; // Cambiado a PascalCase
+import Encuesta from "./Components/Encuesta";
+import Menu from './Components/Menu';
+import NotFound from './Components/NotFound';
+import encuestas from './data/encuestas.json';
 
 function App() {
+  const [listaEncuestas, setListaEncuestas] = useState(encuestas);
+
+  const agregarEncuesta = (nuevaEncuesta) => {
+    nuevaEncuesta.id = listaEncuestas.length + 1;
+    setListaEncuestas([...listaEncuestas, nuevaEncuesta]);
+  };
+
+  const responderEncuesta = (id, respuestas) => {
+    setListaEncuestas(prevEncuestas => {
+      return prevEncuestas.map(encuesta => {
+        if (encuesta.id === parseInt(id)) {
+          encuesta.respuestas = [respuestas];
+        }
+        return encuesta;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu /> {/* Cambiado a componente en PascalCase */}
+      <Routes>
+        <Route path="/" element={<Inicio listaEncuestas={listaEncuestas} />} />
+        <Route path="/encuesta/crear" element={<CrearEncuesta agregarEncuesta={agregarEncuesta} />} />
+        <Route path="/encuesta/:id" element={<Encuesta listaEncuestas={listaEncuestas} responderEncuesta={responderEncuesta} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
